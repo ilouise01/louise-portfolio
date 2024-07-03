859cd4bc6f4e86ea92709b5e70bd77659c931dd2
ilouise01 <d.ivanlouise001@gmail.com>
2024-07-03 20:42:29 +0800

    updated home page, added more works button, created magnetic button effect

--- src/components/Home/HeroWorks/HeroWorks.js
+++ src/components/Home/HeroWorks/HeroWorks.js
@@ -1,115 +1,156 @@
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import SplitType from 'split-type'
-import { useGSAP } from '@gsap/react'
+import { useGSAP } from '@gsap/react';
+import { Link } from 'react-router-dom';
 
-import './HeroWorks.css'
+import './HeroWorks.css';
 import { darkTheme } from '../../../theme';
 
+gsap.registerPlugin(ScrollTrigger);
 
 function HeroWorks() {
-    
-    gsap.registerPlugin(ScrollTrigger);
-    useGSAP(() =>{
-      let mm = gsap.matchMedia();
+  const buttonRef = useRef(null);
 
-      mm.add("(min-width: 501px)", () => {
-        // desktop setup code here...
-        let workanim = gsap.timeline({
-          // yes, we can add it to an entire timeline!
-          scrollTrigger: {
-            trigger: ".works-wrapper",
-            start: "top top",
-            end: "bottom bottom",
-            scrub: 0.3,
-          }
-        });
-        workanim.addLabel("start")
-        workanim.to(".works-bottom",{rotateX: 0, rotateY: 0, rotateZ: 0, top: '40%', }, 'start')
-        workanim.to(".work-canvas",{opacity: 1,}, 'start')
-        workanim.to(".row-1",{translateX: '0rem'}, 'start')
-        workanim.to(".row-2",{translateX: '-10rem'}, 'start')
-        workanim.to(".row-3",{translateX: '0rem'}, 'start')
-        
-        workanim.addLabel("end")
-        workanim.to(".row-1",{translateX: '-35rem'}, 'end')
-        workanim.to(".row-2",{translateX: '20rem'}, 'end')
-        workanim.to(".row-3",{translateX: '-35rem'}, 'end')
-  
-        
+  useGSAP(() => {
+    let mm = gsap.matchMedia();
+
+    mm.add("(min-width: 501px)", () => {
+      // desktop setup code here...
+      let workanim = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".works-wrapper",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: 0.3,
+        }
       });
+      workanim.addLabel("start")
+      workanim.to(".works-bottom", { rotateX: 0, rotateY: 0, rotateZ: 0, top: '40%', }, 'start')
+      workanim.to(".work-canvas", { opacity: 1, }, 'start')
+      workanim.to(".row-1", { translateX: '0rem' }, 'start')
+      workanim.to(".row-2", { translateX: '-10rem' }, 'start')
+      workanim.to(".row-3", { translateX: '0rem' }, 'start')
 
-      mm.add("(max-width: 500px)", () => {
-        // mobile setup code here...
-        let workanim = gsap.timeline({
-          // yes, we can add it to an entire timeline!
-          scrollTrigger: {
-            trigger: ".works-wrapper",
-            start: "top top",
-            end: "bottom bottom",
-            scrub: 0.3,
-          }
-        });
-        workanim.addLabel("start")
-        workanim.to(".works-bottom",{rotateX: 0, rotateY: 0, rotateZ: 0, top: '40%', }, 'start')
-        workanim.to(".work-canvas",{opacity: 1,}, 'start')
-        workanim.to(".row-1",{translateX: '0rem'}, 'start')
-        workanim.to(".row-2",{translateX: '-95rem'}, 'start')
-        workanim.to(".row-3",{translateX: '0rem'}, 'start')
-        
-        workanim.addLabel("end")
-        workanim.to(".row-1",{translateX: '-95rem'}, 'end')
-        workanim.to(".row-2",{translateX: '0rem'}, 'end')
-        workanim.to(".row-3",{translateX: '-95rem'}, 'end')
-  
-        
+      workanim.addLabel("end")
+      workanim.to(".row-1", { translateX: '-35rem' }, 'end')
+      workanim.to(".row-2", { translateX: '20rem' }, 'end')
+      workanim.to(".row-3", { translateX: '-35rem' }, 'end')
+    });
+
+    mm.add("(max-width: 500px)", () => {
+      // mobile setup code here...
+      let workanim = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".works-wrapper",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: 0.3,
+        }
       });
-      
-      
+      workanim.addLabel("start")
+      workanim.to(".works-bottom", { rotateX: 0, rotateY: 0, rotateZ: 0, top: '40%', }, 'start')
+      workanim.to(".work-canvas", { opacity: 1, }, 'start')
+      workanim.to(".row-1", { translateX: '0rem' }, 'start')
+      workanim.to(".row-2", { translateX: '-95rem' }, 'start')
+      workanim.to(".row-3", { translateX: '0rem' }, 'start')
 
-  
-    })
-    useEffect(() => {
+      workanim.addLabel("end")
+      workanim.to(".row-1", { translateX: '-95rem' }, 'end')
+      workanim.to(".row-2", { translateX: '0rem' }, 'end')
+      workanim.to(".row-3", { translateX: '-95rem' }, 'end')
+    });
+  });
 
-      
-        
-    return () => {
-      // cleanup code before component unmounts
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      if (buttonRef.current) {
+        const rect = buttonRef.current.getBoundingClientRect();
+        const distanceX = e.clientX - (rect.left + rect.width / 2);
+        const distanceY = e.clientY - (rect.top + rect.height / 2);
+
+        gsap.to(buttonRef.current, {
+          x: distanceX * 3,
+          y: distanceY * 3,
+          background: '#00a2e2',
+          color: 'black',
+          duration: 0.7,
+          ease: 'power3.out',
+        });
+
+        gsap.to('.works-btn-text', {
+          x: distanceX * 0.25,
+          y: distanceY * 0.25,
+          duration: 3,
+          ease: 'power3.out',
+        });
+      }
     };
+
+    const handleMouseLeave = () => {
+      gsap.to(buttonRef.current, {
+        x: 0,
+        y: 0,
+        background: 'transparent',
+          color: 'white',
+        duration: 3,
+        ease: 'elastic.out',
+      });
+
+
+      gsap.to('.works-btn-text', {
+        x: 0,
+        y: 0,
+        duration: 5,
+        ease: 'elastic.out',
+      });
+    };
+
+    const buttonElement = buttonRef.current;
+    if (buttonElement) {
+      buttonElement.addEventListener('mousemove', handleMouseMove);
+      buttonElement.addEventListener('mouseleave', handleMouseLeave);
+
+      return () => {
+        buttonElement.removeEventListener('mousemove', handleMouseMove);
+        buttonElement.removeEventListener('mouseleave', handleMouseLeave);
+      };
+    }
   }, []);
 
   return (
-    <div className='heroworks'>
-      <div className='works-wrapper'>
-        <div className='works-top'>
-          <h2 className='mons'>Selected Works</h2>
-          <h5 style={{fontWeight: 600,}}>Here are some examples of my work.</h5>
-          <h5 style={{ fontWeight: 500, textAlign: 'center', }}>(Placeholder images used to represent envisioned designs. Actual projects are in progress or pending creation.)</h5>
-        </div>
-        <div className='works-bottom'>
-          <div className='works-row row-1'>
-            <div className='work-canvas' id='wc1'></div>
-            <div className='work-canvas' id='wc2'></div>
-            <div className='work-canvas' id='wc3'></div>
+    <>
+      <div className='heroworks'>
+        <div className='works-wrapper'>
+          <div className='works-top'>
+            <h2 className='mons'>Selected Works</h2>
+            <h5 style={{ fontWeight: 600, }}>Here are some examples of my work.</h5>
+            <h6 style={{ fontWeight: 500, textAlign: 'center', }}>(Some images are placeholders used for representing envisioned designs. Actual projects are in progress or pending creation.)</h6>
           </div>
-          <div className='works-row row-2'>
-            <div className='work-canvas' id='wc4'></div>
-            <div className='work-canvas' id='wc5'></div>
-            <div className='work-canvas' id='wc6'></div>
-          </div>
-          <div className='works-row row-3'>
-            <div className='work-canvas' id='wc7'></div>
-            <div className='work-canvas' id='wc8'></div>
-            <div className='work-canvas' id='wc9'></div>
+          <div className='works-bottom'>
+            <div className='works-row row-1'>
+              <div className='work-canvas' id='wc1'></div>
+              <div className='work-canvas' id='wc2'></div>
+              <div className='work-canvas' id='wc3'></div>
+            </div>
+            <div className='works-row row-2'>
+              <div className='work-canvas' id='wc4'></div>
+              <div className='work-canvas' id='wc5'></div>
+              <div className='work-canvas' id='wc6'></div>
+            </div>
+            <div className='works-row row-3'>
+              <div className='work-canvas' id='wc7'></div>
+              <div className='work-canvas' id='wc8'></div>
+              <div className='work-canvas' id='wc9'></div>
+            </div>
           </div>
         </div>
       </div>
-      
-    </div>
-        
-
-    
+      <div className='more-works'>
+        <h4 style={{textAlign: 'center'}}>Explore all of my work by clicking the button below!</h4>
+        <button className='works-button' ref={buttonRef}><Link style={{textDecoration: 'none'}}  to="/works" ><h5 className='works-btn-text'>More works</h5></Link></button>
+      </div>
+    </>
   );
 }
 
--- src/components/Navs/Navs.js
+++ src/components/Navs/Navs.js
@@ -1,41 +1,91 @@
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-import './Navs.css'
+import './Navs.css';
 import { darkTheme } from '../../theme';
-import BurgerMenu from "../BurgerMenu/BurgerMenu"
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
+gsap.registerPlugin(ScrollTrigger);
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+function Navs({ openburger, burgerOpen, menutransition, menutransition2 }) {
+  const navicons = useRef([]);
 
+  useEffect(() => {
+    const handleMouseMove = (e, icon) => {
+      const rect = icon.getBoundingClientRect();
+      const distanceX = e.clientX - (rect.left + rect.width / 2);
+      const distanceY = e.clientY - (rect.top + rect.height / 2);
 
+      gsap.to(icon, {
+        x: distanceX * 3,
+        y: distanceY * 3,
+        duration: 0.7,
+        ease: 'power3.out',
+      });
 
-function Navs({openburger, burgerOpen,menutransition,menutransition2 }) {
-  
-    useEffect(() => {
      
 
+      gsap.to('.trail' ,{borderColor: '#00a2e2',});
+        gsap.to('.ball',{backgroundColor: '#00a2e2',});
+        gsap.to('.trail',{backgroundColor: '#00a2e2',});
+        gsap.to('.trail' ,{backgroundColor: '#00a2e2', opacity: '1', height: '45px', width: '45px',});
+    };
 
+    const handleMouseLeave = (icon) => {
+      gsap.to(icon, {
+        x: 0,
+        y: 0,
+        duration: 2,
+        ease: 'elastic.out',
+      });
+
+      
+
+      gsap.to('.trail',{borderColor: '#ffffff',});
+      gsap.to('.ball' ,{backgroundColor: '#ffffff',});
+      gsap.to('.trail' ,{backgroundColor: 'transparent', opacity: '1', height: '15px', width: '15px', });
+    };
+
+    navicons.current.forEach(icon => {
+      const onMouseMove = (e) => handleMouseMove(e, icon);
+      const onMouseLeave = () => handleMouseLeave(icon);
+
+      icon.addEventListener('mousemove', onMouseMove);
+      icon.addEventListener('mouseleave', onMouseLeave);
+
+      icon.cleanup = () => {
+        icon.removeEventListener('mousemove', onMouseMove);
+        icon.removeEventListener('mouseleave', onMouseLeave);
+      };
+    });
 
     return () => {
-      // cleanup code before component unmounts
+      navicons.current.forEach(icon => {
+        icon.cleanup();
+      });
     };
   }, []);
 
   return (
-        <div className='navs'>
-            <BurgerMenu openburger={openburger} burgerOpen={burgerOpen} menutransition={menutransition} menutransition2={menutransition2}/>
-            <h4 className='navlouise mons' style={{WebkitTextStrokeColor: darkTheme.Secondary, }}>LOUISE</h4>
-            <div className='socials-container'>
-                <FontAwesomeIcon icon={faGithub} size='2xl'/>
-                <FontAwesomeIcon icon={faLinkedin} size='2xl'/>
-                <FontAwesomeIcon icon={faCircleUser} size='2xl'/>
-            </div>
-        </div>
-        
+    <div className='navs'>
+      <BurgerMenu openburger={openburger} burgerOpen={burgerOpen} menutransition={menutransition} menutransition2={menutransition2} />
+      <h4 className='navlouise mons' style={{ WebkitTextStrokeColor: darkTheme.Secondary }}>LOUISE</h4>
+      <div className='socials-container'>
+        {[faGithub, faLinkedin, faCircleUser].map((icon, index) => (
+          <FontAwesomeIcon
+            key={index}
+            ref={el => navicons.current[index] = el}
+            className='navicon'
+            icon={icon}
+            size='2xl'
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
